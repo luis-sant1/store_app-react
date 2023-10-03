073ae68f66cb842e56fc8bf4c0abaee28d90bea2
DAW-DUCKASWELL <santiagomuchacholuma@uvm.edu.ve>
2023-10-02 23:16:58 -0600

    Fixed Descriptions.
    Fix Descriptions

--- Api/db/dbPokemons.js
+++ Api/db/dbPokemons.js
@@ -16,7 +16,7 @@ function addPokemons() {
          categoria: ['Ratón'],
       }, {
          nombre: 'Charmander',
-         descripcion: "Charmander es un pequeño monstruo bípedo parecido a un lagarto. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola, cuya punta está envuelta en llamas. Charmander y sus evoluciones, Charmeleon y Charizard, tienen una pequeña llama en la punta de sus colas desde que nacen. La intensidad con la que ésta arde es un indicador del estado de salud y emocional de este Pokémon: si la llama arde con mucha fuerza, indica que está completamente sano, y si arde muy levemente, significa que se encuentra débil. El Pokémon podría morir si la llama de su cola se apaga. Cuando son bebés aún no están familiarizados con el fuego, pudiendo llegar a quemarse a sí mismos.",
+         descripcion: "Charmander es un pequeño monstruo bípedo parecido a un lagarto. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola, cuya punta está envuelta en llamas. Charmander y sus evoluciones, Charmeleon y Charizard, tienen una pequeña llama en la punta de sus colas desde que nacen.",
          imagen: {
             public_id: 'PokemonsImage/1696238476135-Charmander',
             secure_url:
@@ -27,7 +27,7 @@ function addPokemons() {
          categoria: ['Lagartija'],
       }, {
          nombre: 'Squirtle',
-         descripcion: "Squirtle tiene forma de una tortuga semiacuática de una tonalidad azulada, su caparazón es color café, las placas periféricas de color blanco y finalmente su plastrón de una tonalidad crema, posee una cola con la punta enrollada, además de tres dedos en cada una de sus extremidades, una boca con una punta en forma de pico característico de las tortugas y unos grandes ojos de tonalidad rojiza.",
+         descripcion: "Squirtle tiene forma de una tortuga semiacuática de una tonalidad azulada, su caparazón es color café, las placas periféricas de color blanco y finalmente su plastrón de una tonalidad crema, posee una cola con la punta enrollada, además de tres dedos en cada una de sus extremidades.",
          imagen: {
             public_id: 'PokemonsImage/1696238536040-Squirtle',
             secure_url:
