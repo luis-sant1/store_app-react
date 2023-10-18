3e09a0cab65c6dd0a5b2c85bfa40ba2235b0842e
DAW-DUCKASWELL <114958225+luis-sant1@users.noreply.github.com>
2023-10-17 18:23:40 -0600

    Add new components, modified Home, Add customHooks
    Nuevos componentes de ayuda creados, creado un customHook para hacer el fetching de datos.

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -5,47 +5,47 @@ import CardPokemons from "../CardPokemons/CardPokemons";
 import { useModal } from "../Modal/useModal"
 import Modal from "../Modal/Modal";
 import CardDetail from "../CardDetail/CardDetail";
+import { useFetch } from "../customHooks/useFetch"
+import { Loading } from "../extraComponents/Loading";
+import { Error } from "../extraComponents/Error";
+import { Pagination } from "../extraComponents/pagination";
 
-export default function Home(props){
-    const funUp=props
-    console.log(funUp)
+export default function Home(props) {
+    const funUp = props
     const [isOpenAlert, openAlert, closeAlert] = useModal(false);
-    const [dataDetail,setDataDetail]=useState({})
-    const [dataPokemons, setdataPokemons] = useState([]);
-    const Potions = async()=>{
-        try {
-            const response  = await axios(`http://localhost:3000/pokemons`)
-            const dataPokemons = Object.values(response.data);
-            setdataPokemons(dataPokemons);
-            console.log(dataPokemons);
-    
-        } catch (e){
-            console.log(e);
-        }
-    }
-    useEffect(() => {
-        Potions();
-    }, []);
-    const openDetail=(e, data)=>{
+    const [dataDetail, setDataDetail] = useState({})
+    const [products, setProducts] = useState([]);
+    const [productPerPage, setProductPerPage] = useState(6)
+    const [currentPage, setCurrentPage] = useState(1)
+
+
+    const { data, loading, error } = useFetch(`https://api.escuelajs.co/api/v1/products?offset=0&limit=9`);
+
+    const openDetail = (e, data) => {
         setDataDetail(data)
         openAlert()
     }
-    console.log(dataDetail)
-    return(
+
+
+
+    return (
         <div className="Po">
             <Modal isOpen={isOpenAlert} onClose={closeAlert}>
-                        <CardDetail dataDetail={dataDetail} funUp={funUp}/>
+                <CardDetail dataDetail={dataDetail} funUp={funUp} />
             </Modal>
             <div className="items-center justify-center text-center mt-4 text-white">
-                    <span className="text-3xl font-bold pt-4 text-white"><h1>POKEMONES</h1></span>
+                <span className="text-3xl font-bold pt-4 text-white"><h1>POKEMONES</h1></span>
             </div>
-                <div className="flex p-8 justify-center">
-                    <div className="w-auto flex flex-wrap lg:gap-14 place-items-center m-8 pb-4 mb-8">
-                            {dataPokemons.map((e)=>(
-                                <CardPokemons dataPokemons={e} openDetail={openDetail}/>
-                            ))}
-                    </div>
+            <div className="flex p-8 justify-center">+
+                <div className="w-auto flex flex-wrap lg:gap-14 place-items-center m-8 pb-4 mb-8">
+                    {error && <Error error={error} />}
+                    {loading && <Loading />}
+                    {data?.map((e) => (
+                        <CardPokemons key={data.id} data={e} openDetail={openDetail} />
+                    ))}
+                    <Pagination />
                 </div>
+            </div>
         </div>
     )
 }
