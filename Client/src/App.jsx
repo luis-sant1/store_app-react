7e26d622b264f8527c1409a79852aad86d458e53
Sebas04pm <92651006+Sebas04pm@users.noreply.github.com>
2023-10-18 22:48:31 -0400

    Agregando importaciones y barra de busqueda

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Home from './components/Home/Home'
 import CreatePokemon from './components/CreatePokemon/CreatePokemon' 
 import UpdatePokemon from './components/UpdatePokemon/UpdatePokemon'
 import logo from './images/pokemon.png'
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
   const [page,setPage]=useState('home')
@@ -30,10 +34,12 @@ function App() {
   }
   return (
     <div>
-      <header className="grid grid-cols-3 bg-white h-14">
+      <header className="grid grid-cols-6 bg-white h-14">
         <img src={logo} alt="img_not_fund" className='ml-4 w-14 h-14' />
         <button onClick={toPage("home")} className='rounded-lg shadow-lg bg-red-400 m-2 p-1 text-sm font-medium'>Inicio</button>
         <button onClick={toPage("create")} className='rounded-lg shadow-lg bg-yellow-300 m-2 p-1 text-sm font-medium'>Crear</button>
+        <input class="w-64 h-10 pl-2 pr-8 rounded-l-full focus:outline-none m-2 p-1" type="text" onChange={handleChange} placeholder="Buscar..." />
+        <button className="btn btn-success"><FontAwesomeIcon icon={faSearch}/></button>
       </header>
       {getContent()}
     </div>
