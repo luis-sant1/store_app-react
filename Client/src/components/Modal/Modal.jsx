fae6fc11df676d660b267ac99827de99466498d5
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-10-20 21:07:09 -0600

    Add comments

--- Client/src/components/Modal/Modal.jsx
+++ Client/src/components/Modal/Modal.jsx
@@ -1,11 +1,11 @@
 import "./modal.css"
 
-const Modal = ({ children, isOpen, onClose }) => {
+const Modal = ({ children, isOpen, onClose }) => { // Función que maneja la toast-page
     const handlerStopPropagation= (e)=> e.stopPropagation();
     return (
-        <div className={`modal ${isOpen && "is-open"}`} onClick={onClose} >
-            <div className="modal-container" onClick={handlerStopPropagation}>
-                <button className="modal-close" onClick={onClose}>X</button>
+        <div className={`modal ${isOpen && "is-open"}`} onClick={onClose} > {/*Cierra si se clickea fuera */}
+            <div className="modal-container" onClick={handlerStopPropagation}> 
+                <button className="modal-close" onClick={onClose}>X</button> 
                 {children} 
             </div>
         </div>
